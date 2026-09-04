88082f96e5bff2619bdb005872745fe546df2fda
ivanes921 <ivanes921@gmail.com>
2026-09-05 00:56:55 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='wlan-magic-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
